120fe98ca7e82fd85ed55efa48e9be5ff186c1c0
Deivu (Saya) <36309350+Deivu@users.noreply.github.com>
2019-02-12 21:40:34 +0800

    one liner index for thuglife

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-const fs = require('fs')
-module.exports = {
-    Haruna: require('./src/haruna.js'),
-    version: JSON.parse(fs.readdirSync('./package.json')).version
-}
+module.exports = require('./src/haruna.js')
