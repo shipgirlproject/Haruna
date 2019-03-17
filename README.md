bdaa9933597092a3b1571832ad44b393e4d42285
takase1121 <20792268+takase1121@users.noreply.github.com>
2019-03-18 00:06:18 +0800

    dev

--- README.md
+++ README.md
@@ -5,82 +5,55 @@
 
 The ShipGirl Project. Haruna, the helping hand of Kashima. ``(c) Kancolle for Haruna.``
 
-Simple webhook vote handler for Discord Bot List https://discordbots.org/ to help with Kashima https://discordbots.org/bot/424137718961012737
+Simple webhook vote handler for [Discord Bot List](https://discordbots.org/) to help with [Kashima](https://discordbots.org/bot/424137718961012737)
 
 Why Haruna? Cause Haruna is cute uwu.
 
-This api / webhook vote handler is oversimplified. As long as you can start it, it will handle everything for you to save you the hassle of creating your own vote handler.
+This API / webhook handler is ~~oversimplified~~simple to use. As long as you can start it, it will handle everything for you to save you the hassle of creating your own vote handler.
+> Now even comes with [`server.js`](https://github.com/deivu/haruna/blob/master/server.js), all you need to run to have your vote handler ready!
 
 ## API Endpoints
-### /vote/ "POST"
+### `POST` /vote
 This is the one that you use for DBL, this is where DBL will send the votes from your bot.
 
 <p align="center">
   <img src="https://i.imgur.com/fBhIdVC.jpg">
 </p>
 
-### /hasVoted/ "GET"
-This is where you check if someone voted or not.
+### `GET` /hasVoted
+To check if someone voted or not.
+```
+HTTP Headers:
+  <String> authorization - Authorization key
+  <String> user_id - User ID to check
+  <Boolean> checkWeekend - (Optional) CHeck if vote multiplier on this user is enabled (weekend special)
+Return value:
+  <Boolean> - `true` if user voted or vote multiplier is enabled, `false` if user did not vote.
+```
 
-`Needs the FF headers`
-
-`"authorization": "Same on what you used for your DBL webhook password"`
-
-`"checkWeekend": "(optional) boolean, true or false. If you want to check if the user voted in weekend"`
-
-`"user_id": "ID of the user you want to check"`
-
-Returns "TRUE" if the user voted, "FALSE" if not.
-
-### /getVotedTime/ "GET"
-This is if you want to check how long the user will stay in database.
-
-`Needs the FF headers`
-
-`"authorization": "Same on what you used for your DBL webhook password"`
-
-`"user_id": "ID of the user you want to check"`
-
-Returns the "MS" of how long the user will be in cache, "FALSE" if the user haven't voted.
+### `GET` /getVotedTime
+To check how long the user will stay in database.
+```
+HTTP Headers:
+  <String> authorization - Authorization key
+  <String> user_id - User ID to check
+Return value:
+  <Number|Boolean> - The duration of how long the user will be in cache (in ms), `false` if the user haven't voted.
+```
 
 ## Some Documentation?
-Class Haruna
-
-Constructor
-```js
-new Haruna(dbl_webhook_port, dbl_webhook_pw, user_lifetime_in_db_in_ms, dbl_token, db_location<optional>)
-```
-Properties
-```
-port: port you used in the api
-auth: auth you specified for both GET and POST requests
-length: user lifetime you specifed in MS before they get deleted
-dbl_token: your bot token in DBL (only used to verify who is the user who voted)
-```
-
-Methods
-```js
-setWebhook(webhook_id, webhook_token) 
- > Sets the webhook where Haruna will send a notifcation when someone votes, when the vote service is online and when she executes her cron job for clearing users. Returns Nothing and not a promise
-```
+[Code Documentation]()
 
 ## Example Code in starting the API
-```js
-const Haruna = require('./index.js')
-const handler = new Haruna('open_dblwebhook_port', 'dblwebhook_password', 'user_lifetime_in_db', 'dbl_token')
-handler.setWebhook('discord_webhook_id', 'discord_webhook_token')
-```
+Check [`server.js`](https://github.com/deivu/haruna/blob/master/server.js)
 
 
 ## How to use this?
-`1. Clone this Repo`
+1. Clone this repo
+2. Make a file `config.json` that contains options for Haruna
+3. Run [`server.js`](https://github.com/deivu/haruna/blob/master/server.js)
+4. If you think [`server.js`](https://github.com/deivu/haruna/blob/master/server.js) is not enough, then you should use your own parent process, or maybe running it on your bot (not recommended)
 
-`2. Put it on a folder separated from your bot or along side your bot (If you have a server, If not then cloning it to a hosting like glitch will do, skip this step if thats the case)`
-
-`3. Copy and modify the example code ABOVE as you see fit for starting the api`
-
-`4. Look on client.js in examples folder to see how you send a request to use this api.`
-
-You may want to fire it up with pm2 or something so that it will not close, as well restart it if it crashes due to some reason
-
-`Note: This process will run SEPARATELY from your bot. It's in its own process, so even your bot crashes, the api won't`
+## Notes
+* You might want to run it with [`pm2`](http://pm2.keymetrics.io/) or system services so any unexpected restarts will be handled and better logging
+* This is a standalone API, so as long as the parent process is alive, it should not die.
--- src/haruna.js
+++ src/haruna.js
@@ -1,31 +1,57 @@
-const fastify = require('fastify')()
+const fastify = require('fastify')
 const enmap = require('enmap')
 const fetch = require('node-fetch')
 const { WebhookClient } = require('discord.js')
 const { version } = require('../package.json')
+const defaultSettings = {
+    port: 8000,
+    auth: '',
+    length: 43200000,
+    token: '',
+    dir: './data',
+    https: null,
+    webhook:''
+}
+
 class Haruna {
-    constructor(port, auth, length, dbl_token, dir) {
-        if (!port || !auth || !length || !dbl_token)
-            throw new Error('One of the required constructors are missing, Please make sure they are set correctly and try again')
+    /**
+     * Starts the server
+     * @param {Object} options The options
+     * @param {number} [options.port=8080] The port to listen to
+     * @param {string} options.auth Authentication key. Will be used when querying the server
+     * @param {number} [options.length=43200000] A vote's lifetime in miliseconds. Defaults to 12 hours
+     * @param {string} options.token DBL token
+     * @param {string} [options.dir='./'] The directory of the database
+     * @param {https} options.https HTTPS option for fastify. Omit this if you dont use it
+     * @param {webhook} options.webhook The webhook **link**
+     */
+    constructor(options) {
+        this.options = {...defaultSettings, ...options}
+        if (!this.options.auth || !this.options.token)
+            throw new Error('Authentication key or DBL token not specified')
+
+        if (this.options.https) {
+            this.app = fastify({https: this.options.https})
+        } else {
+            this.app = fastify()
+        }
+        
         this.storage = new enmap({
             name: 'votes',
             fetchAll: true,
-            dataDir: dir ? dir : ''
+            dataDir: this.options.dir
         })
-        this.port = port
-        this.auth = auth
-        this.length = length
-        this.dbl_token = dbl_token
-        /*
-        * 
-        *
-        */
+
         this.storage.defer.then(() => {
-            fastify.post('/vote/', (req, res) => this._onVote(req, res))
-            fastify.get('/hasVoted/', (req, res) => this._onCheck(req, res))
-            fastify.get('/getVotedTime/', (req, res) => this._onCheckInfo(req, res))
-            fastify.listen(port, '0.0.0.0', () => console.log(`[Notice] Haruna's Vote Service is now Online, listening @ ${port}`))
-            setInterval(() => {
+            this.app.post('/vote', (req, res) => this._onVote(req, res))
+            this.app.get('/hasVoted', (req, res) => this._onCheck(req, res))
+            this.app.get('/getVotedTime', (req, res) => this._onCheckInfo(req, res))
+            this.app.get('/', (req, res) => this._index(req, res))
+            this.app.listen(this.options.port)
+                .catch(err => err && console.error('[Error] Server failed to start. Details: ', err.message))
+                .then(addr => console.log("[Notice] Haruna's Vote Service is now Online, listening @ ", addr))
+
+            setInterval(async () => {
                 let counter = 0;
                 for (const [key, val] of this.storage) {
                     if (val.time < Date.now()) {
@@ -34,7 +60,7 @@ class Haruna {
                     }
                 }
                 console.log(`[Cron Job] Database Purged, removed ${counter} ${counter <= 1 ? 'user' : 'users'} from vote db`)
-                if (this.webhook) this.webhook.send({
+                if (this.options.webhook) this.execWebhook({
                     embeds: [{
                         color: 0x9B767B,
                         description: `📤 Cleaned **${counter} ${counter <= 1 ? 'user' : 'users'}** from database`,
@@ -44,37 +70,37 @@ class Haruna {
                         }
                     }]
                 }).catch(console.error)
+
+                if (this.options.webhook) this.execWebhook({
+                    embeds: [{
+                        color: 0x90ee90,
+                        description: '✅ **Voting API intialized**',
+                        timestamp: new Date(),
+                        footer: {
+                            text: `Haruna Vote Handler v${version}`
+                        }
+                    }]
+                }).catch(console.error)
             }, 300000)
         })
     }
 
-    setWebhook(id, token) {
-        if (this.webhook) throw new Error('Webhook already running')
-        this.webhook = new WebhookClient(id, token)
-        if (this.webhook) this.webhook.send({
-			embeds: [{
-                color: 0x90ee90,
-                description: '✅ **Voting API intialized**',
-                timestamp: new Date(),
-                footer: {
-                    text: `Haruna Vote Handler v${version} `
-                }
-			}]
-		}).catch(console.error)
+    execWebhook(data) {
+        return fetch(this.options.webhook, { method: 'POST', body: JSON.stringify(data), headers: { 'Content-Type': 'application/json' } })
     }
 
     async _fetch_user(id) {
         const req = await fetch(`https://discordbots.org/api/users/${id}`, {
-            headers: { authorization: this.dbl_token }
+            headers: { authorization: this.options.token }
         })
         const user = await req.json()
         return user.username + '#' + user.discriminator
     }
 
     async _send_new_vote_embed(user_id) {
-        if (!this.webhook) return 
+        if (!this.options.webhook) return
         const tag = await this._fetch_user(user_id)
-        await this.webhook.send({
+        await this.execWebhook({
             embeds: [{
                 color: 0x095562,
                 description: `📥 New User: **@${tag}** (${user_id}) voted`,
@@ -87,13 +113,12 @@ class Haruna {
     }
 
     _onVote(req, res) {
-        if (req.headers.authorization !== this.auth) {
+        if (req.headers.authorization !== this.options.auth) {
             res.status(401).send('Unauthorized')
-            console.log('[Notice] Rejected Post Request, Details Below')
-            console.log(req.headers)
+            console.log('[Notice] Rejected Post Request, Details Below\n', req.headers)
         } else {
             if (this.storage.has(req.body.user)) this.storage.delete(req.body.user)
-            const duration = Date.now() + this.length
+            const duration = Date.now() + this.options.length
             this.storage.set(req.body.user, { time: duration, isWeekend: req.body.isWeekend})
             res.send('Sucess')
             console.log(`[Notice] New vote stored, Duration: ${Math.floor((duration - Date.now()) / 1000 / 60 / 60)} hrs, user_id: ${req.body.user}, isWeekend: ${req.body.isWeekend}.`)
@@ -102,17 +127,16 @@ class Haruna {
     }
 
     _onCheck(req, res) {
-        if (req.headers.authorization !== this.auth) {
+        if (req.headers.authorization !== this.options.auth) {
             res.status(401).send('Unauthorized')
-            console.log('[Notice] Rejected hasVoted Request, Details below')
-            console.log(req.headers)
+            console.log('[Notice] Rejected hasVoted Request, Details below\n', req.headers)
         } else {
             const user = this.storage.get(req.headers.user_id)
             if (req.headers.user_id) {
                 if (req.headers.checkWeekend) {
                     res.send(user && user.isWeekend)
                 } else {
-                    res.send(user ? true : false)
+                    res.send(!!user)
                 }
             } else res.send(false)
             console.log(`[Notice] Checked Vote for user_id ${req.headers.user_id}. Time Left: ${user ? `${((user.time - Date.now()) / 1000 / 60 / 60).toFixed(1)} hr(s).`: 'Not in Database.'}`)
@@ -120,10 +144,9 @@ class Haruna {
     }
 
     _onCheckInfo(req, res) {
-        if (req.headers.authorization !== this.auth) {
+        if (req.headers.authorization !== this.options.auth) {
             res.status(401).send('Unauthorized')
-            console.log('[Notice] Rejected getVotedTime Request, Details below')
-            console.log(req.headers)
+            console.log('[Notice] Rejected getVotedTime Request, Details below\n', req.headers)
         } else {
             const user = this.storage.get(req.headers.user_id)
             if (user && req.headers.user_id) {
@@ -131,6 +154,10 @@ class Haruna {
             } else res.send(false)
             console.log(`[Notice] Checked Vote Time for user_id ${req.headers.user_id}. Time Left: ${user ? `${((user.time - Date.now()) / 1000 / 60 / 60).toFixed(1)} hr(s).`: 'Not in Database.'}`)
         }
+    }
+
+    _index(req, res) {
+        res.send(`Haruna Vote Handler v${version}`)
     }
 }
 module.exports = Haruna
